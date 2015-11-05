372f2491174c1840a80394af7ac5adcced4209c9
Rob Archer <robert_archer@wgbh.org>
2015-11-05 18:57:19 -0500

    assets being served correctly

---
+++ app/assets/stylesheets/index.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<title>Welcome to Tales of Valor</title>
+<style>
+/* Tales of Valor CSS Created 2009 by Larry Joyce */
+/* CSS formatting for Wide theme */
+
+/* Div Control */
+#page {
+	background-color:#003;
+ 	background-image:url(http://characters.talesofvalor.com/stylesheets/img/paper017.jpg);
+	width: 1300px;
+	margin: 0 auto;
+	border: 5px solid #000;
+}
+#tip_top {height:25px;
+}
+#tt_right {
+	float:right;
+	width:250px;
+	}
+#tt_left {
+	float:left;
+	width:1000px;
+	}
+#top {
+	height:150px;
+}
+.header {
+	float: left;
+	width: 900px;
+	background: url(http://characters.talesofvalor.com/stylesheets/img/header2.jpg) no-repeat;
+	font:2.4em;
+	line-height: 150px;
+	text-align: center;
+	margin-left: 5px;
+}
+
+#actionbox {
+	float:right;
+	border:#5E5E5E dashed 2px;
+	width:350px;
+	height:125px;
+	padding:1px;
+	margin-right:25px;	
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+}
+#filterbox {
+	float:left;
+	border:#5E5E5E dashed 2px;
+	width:650px;
+	height:125px;
+	padding:1px;
+		-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+}
+#picturebox {
+	width:200px;
+	float:right;
+	padding:5px;
+		-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+}
+#afbox {
+	width:720px;
+}
+.center {
+	width:1110px;
+	padding-left:25px;
+	float:left;
+	margin-top:1px;
+	background-image:url(http://characters.talesofvalor.com/stylesheets/img/paper_background.png);
+
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+
+}
+/* Login Section */
+.login {
+	background-image:url(http://characters.talesofvalor.com/stylesheets/img/paper_background.png);
+	width: 300px;
+	float:right;
+	border:#5E5E5E dashed 2px;
+	padding:1em;
+	margin-right:5px;
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+}
+
+
+/* Center Top Menu Bar */
+#menu {
+	width: 1300px;
+	height: 40px;
+	margin: 0 auto;
+}
+
+
+/* Left Menu Structure */
+.menuleft {
+	width:124px;
+	float:left;
+	padding-left:1px;
+	-moz-border-radius-bottomright: 10px;
+	-webkit-border-bottom-right-radius: 10px;
+}
+
+
+/* Event Section */
+#eventinfo{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+#eventwindow {
+	width:1050px;
+	float:left;
+	border:#5E5E5E dotted 2px;
+	padding:1em;
+	margin-top:2px;
+	margin-bottom:2px;
+}
+#eventctrl {
+	width:500px;
+	border:none;
+}
+
+
+/* Character & Player Sections */
+#charlisting {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	border:#666 dashed 2px;
+}
+#chevents{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+#eventreg{
+	width:1050px;
+		margin-left:50px;
+		background-image:url(http://characters.talesofvalor.com/stylesheets/img/img01.jpg);
+		border:#666 dashed 2px;
+}
+#playerlog{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+/* Character Display */
+#chartop {
+		float:left;
+	width:1050px;
+}
+#chardesc {float:left;
+width:700;
+}
+#chphoto {
+		width:200px;
+		float:right;
+		margin-right:10px;
+}
+#charmain {
+		float:left;
+	width:1050px;
+		margin-top:2px;
+}
+.charul{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	margin-left:50px;
+}
+
+/* Info Section */
+#wacp {
+	width:1050px;
+	float:left;
+	margin-top:1px;
+	padding-left:10px;
+	padding-right:30px;
+}
+#newplayer {
+	width:1050px;
+	float:left;
+	margin-top:1px;
+	padding-left:10px;
+	padding-right:30px;
+}
+#vetlarp {
+	width:1050px;
+	float:left;
+	margin-top:1px;
+	padding-left:10px;
+	padding-right:30px;
+}
+#infobox {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	padding-left:10px;
+	padding-right:30px;
+}
+#makeup {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	padding-left:10px;
+	padding-right:30px;
+}
+#pguide {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	padding-left:10px;
+	padding-right:30px;
+}
+/* Game Information Section */
+#gameinfo {
+	width:1050px;
+	float:left;
+	margin-top:1px;
+	padding-top:10px;
+	padding-right:2px;
+}
+
+
+
+/* Tag Section */
+#tagshow {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+#taglist{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+#tagleft {
+	width:400px;
+	float:left;
+	margin-top:2px;
+}
+#tagright {
+	width:400px;
+	float:left;
+	margin-top:2px;
+	margin-left:25px;
+}
+#tagmain {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+.tagul{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	margin-left:50px;
+}
+#tagcenter {
+	width:1050px;
+	float:left;
+}
+
+
+/* Skill Section */
+#skillshow {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+#skilltree{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+.treeopen {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	padding-left:10px;
+}
+
+.treeopen ul {
+	float:left;
+	width:50em;
+	margin:0;
+	padding:0;
+	list-style:none;
+	margin-bottom:10px;
+}
+.treeopen li {
+	float:left;
+	width:25em;
+	margin:0;
+	padding:0;
+}
+
+.treeul{
+	width:1050px;
+	float:left;
+	margin-top:2px;
+	padding-left:10px;
+	}
+
+.treeul ul {
+	float:left;
+	width:1050px;
+	margin:0;
+	margin-bottom:10px;
+	padding:0;
+	list-style:none;
+}
+.treeul li {
+	float:left;
+	width:280px;
+	margin:0;
+	padding:0;
+	font-size:0.9em;
+}
+/* Player's Guide Section */
+#pguide {
+	width:1050px;
+	float:left;
+	margin-top:2px;
+}
+
+
+/* Information Section Formatting
+
+/*PELs Section Formating */
+.pelul{
+	width:1000px;
+	float:left;
+	margin-top:2px;
+	padding-left:5px;
+	}
+
+.pelul ul {
+	float:left;
+	width:1000px;
+	margin:0;
+	margin-bottom:10px;
+	padding:0;
+	list-style:none;
+	border-bottom-color:#003;
+	border-bottom-style:dashed;
+	border-bottom-width:1px;
+}
+.pelul li {
+	float:left;
+	width:250px;
+	margin:0;
+	padding-bottom:2px;
+	font-size:0.9em;
+}
+</style>
+
+</head>
+<body>
+<div id="page">
+	<div id="tip_top">
+		<div id="tt_left"></div>
+		<div id="tt_right"></div>
+	</div>
+ 	<div id="top">
+    	<div class="header">&nbsp;
+		</div>
+    	<div class="login">
+    	  
+
+      
+	   </div>
+	</div>
+    <div id="menu">
+		<ul>
+          <li><a href="http://characters.talesofvalor.com/pages/intro">INFORMATION</a></li>
+          <li><a href="http://brightwater.talesofvalor.com" target="_new">FORUM</a></li>
+          <li><a href="http://characters.talesofvalor.com">CHARACTER DATABASE</a></li>
+          <li><a href="http://characters.talesofvalor.com/events/">EVENTS</a></li>
+          <li><a href="http://characters.talesofvalor.com/pages/rulesd">RULES</a></li>
+          <li><a href="http://characters.talesofvalor.com/pages/pguide1">PLAYER'S GUIDE</a></li>
+     	</ul>
+    </div>
+    <div class="menuleft">
+      <li>
+      	<h3>Event Registration</h3>
+      	<p>You must log in in order to register</p>
+		<ul class="links">
+			<li><a href="http://characters.talesofvalor.com/events/registration">Event Registration</a></li> 
+		</ul>
+ 
+        <h3>Basics</h3>
+        <ul class="links">
+          <li><a href="http://characters.talesofvalor.com/origins/">Backgrounds</a></li>
+          <li><a href="http://characters.talesofvalor.com/headers/">Headers</a></li>
+          <li><a href="http://characters.talesofvalor.com/skills/">Skills</a></li>
+          <li><a href="http://characters.talesofvalor.com/skills/tree">Skill Tree</a></li>
+         
+        </ul>
+      </li>
+
+      
+    </div>
+    <div class="center">
+      <div id="flash">
+        <p></p>
+      </div>
+      <img src="http://characters.talesofvalor.com/images/Logo.gif"><font size="+2"><b>Tales of Valor Character Database</b></font>
+
+
+</div>
+    <div class="footer">
+      <table width="100%" align="center">
+        <tr>
+		<td align="Center">Copyright 2014:  Tales of Valor : Sean Conlon & Erica McIntyre</td>
+		</tr>
+      </table>
+<table width="100%" align="center">
+<tr>
+<td width="33%" align="Left">Help us improve our game:</td><td>
+<td width="34%" align="center"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="2427648">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form> </td>
+<td width="33%" align="Right"></td>
+		</tr>
+		</tr>
+</table>
+    </div>
+  </div>
+
+</body>
+</html>
